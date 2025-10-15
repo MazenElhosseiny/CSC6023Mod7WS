f25a9e6f1898048639b57e8af76d9418493e5d12
MazenElhosseiny <mhos2014@gmail.com>
2025-10-15 02:14:15 -0400

    Add files via upload

---
+++ ElhosseinyMazenMod7WS.py
@@ -0,0 +1,208 @@
+# Create a tree node
+class TreeNode(object):
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        self.height = 1
+
+class AVLTree(object):
+
+    # Function to see if a node exists
+    def nodeExists(self, root, data): #
+        if not root: # if the node does not exist
+            return False
+        elif data == root.data: # if the node exists
+            return True
+        elif data < root.data: # searches to the left
+            return self.nodeExists(root.left, data)
+        elif data > root.data: # searches to the right
+            return self.nodeExists(root.right, data)
+
+    # Function to insert a node
+    def insert_node(self, root, data):
+
+        # Find the correct location and insert the node
+        if not root:
+            return TreeNode(data)
+        elif data < root.data:
+            root.left = self.insert_node(root.left, data)
+        else:
+            root.right = self.insert_node(root.right, data)
+
+        root.height = 1 + max(self.getHeight(root.left),
+                              self.getHeight(root.right))
+
+        # Update the balance factor and balance the tree
+        balanceFactor = self.getBalance(root)
+        if balanceFactor > 1:
+            if data < root.left.data:
+                return self.rightRotate(root)
+            else:
+                root.left = self.leftRotate(root.left)
+                return self.rightRotate(root)
+
+        if balanceFactor < -1:
+            if data > root.right.data:
+                return self.leftRotate(root)
+            else:
+                root.right = self.rightRotate(root.right)
+                return self.leftRotate(root)
+
+        return root
+
+    # Function to delete a node
+    def delete_node(self, root, data):
+
+        # Find the node to be deleted and remove it
+        if not root:
+            return root
+        elif data < root.data:
+            root.left = self.delete_node(root.left, data)
+        elif data > root.data:
+            root.right = self.delete_node(root.right, data)
+        else:
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+            temp = self.getMinValueNode(root.right)
+            root.data = temp.data
+            root.right = self.delete_node(root.right,
+                                          temp.data)
+        if root is None:
+            return root
+
+        # Update the balance factor of nodes
+        root.height = 1 + max(self.getHeight(root.left),
+                              self.getHeight(root.right))
+
+        balanceFactor = self.getBalance(root)
+
+        # Balance the tree
+        if balanceFactor > 1:
+            if self.getBalance(root.left) >= 0:
+                return self.rightRotate(root)
+            else:
+                root.left = self.leftRotate(root.left)
+                return self.rightRotate(root)
+        if balanceFactor < -1:
+            if self.getBalance(root.right) <= 0:
+                return self.leftRotate(root)
+            else:
+                root.right = self.rightRotate(root.right)
+                return self.leftRotate(root)
+        return root
+
+    # Function to perform left rotation
+    def leftRotate(self, z):
+        y = z.right
+        T2 = y.left
+        y.left = z
+        z.right = T2
+        z.height = 1 + max(self.getHeight(z.left),
+                           self.getHeight(z.right))
+        y.height = 1 + max(self.getHeight(y.left),
+                           self.getHeight(y.right))
+        return y
+
+    # Function to perform right rotation
+    def rightRotate(self, z):
+        y = z.left
+        T3 = y.right
+        y.right = z
+        z.left = T3
+        z.height = 1 + max(self.getHeight(z.left),
+                           self.getHeight(z.right))
+        y.height = 1 + max(self.getHeight(y.left),
+                           self.getHeight(y.right))
+        return y
+
+    # Get the height of the node
+    def getHeight(self, root):
+        if not root:
+            return 0
+        return root.height
+
+    # Get balance factor of the node
+    def getBalance(self, root):
+        if not root:
+            return 0
+        return self.getHeight(root.left) - self.getHeight(root.right)
+
+    def getMinValueNode(self, root):
+        if root is None or root.left is None:
+            return root
+        return self.getMinValueNode(root.left)
+
+    def getMaxValueNode(self, root):
+        if root is None or root.right is None:
+            return root
+        return self.getMaxValueNode(root.right)
+
+    def printPreOrder(self, root):
+        if not root:
+            return
+        print(root.data, end=" ")
+        self.printPreOrder(root.left)
+        self.printPreOrder(root.right)
+
+    def printInOrder(self, root):
+        if not root:
+            return
+        self.printInOrder(root.left)
+        print(root.data, end=" ")
+        self.printInOrder(root.right)
+
+    # Print the tree
+    def printHelper(self, currPtr, indent, last):
+        if currPtr != None:
+            print(indent, end="")
+            if last:
+                print("R----", end="")
+                indent += "     "
+            else:
+                print("L----", end="")
+                indent += "|    "
+            print(currPtr.data)
+            self.printHelper(currPtr.left, indent, False)
+            self.printHelper(currPtr.right, indent, True)
+
+def main():
+    myTree = AVLTree()
+    root = None # starts with empty tree
+    while True: # keeps the program running until user wants to exit
+        while True: # keeps asking user for a number to store/delete in tree or to exit program
+            choice = input("Enter a positive integer to store it in the tree, if it already exists in the tree then it will be deleted. Enter 0 or a negative number to terminate the program: ")
+            if not choice:
+                print("No input received.")
+            else:
+                try:
+                    choice_int = int(choice) # user input
+                    break
+                except ValueError:
+                    print("Invalid input.")
+
+        if choice_int > 0:  # Adds or deletes node from the tree
+            result = myTree.nodeExists(root, choice_int) # depending on if result is true or false we will be able to either insert the node or delete it from the tree
+            if result is False:
+                root = myTree.insert_node(root,choice_int)
+                print("After Insertion of {}: ".format(choice_int))
+            else:
+                root = myTree.delete_node(root, choice_int)
+                print("After Deletion of {}: ".format(choice_int))
+            myTree.printHelper(root, "", True)  # prints the tree
+        else:   # terminates the program
+            print("Goodbye, thanks for joining us.")
+            break
+
+main()
+
+"""
+Report:
+    The algorithm runs in O(logn) due to nodeExists function searching for the value of the data in the current tree. If it finds a match of the data with a value in the tree it returns True, otherwise we return False. Then the insertion or deletion functions are called which also run in O(logn). 
+"""
